81273eba3d3ad970c5cc7f9e5531e97cd5c21e5f
Deepglint-PC <huangyan9188@163.com>
2014-12-16 12:58:39 +0800

    revise the nsq_to_mongo

--- apps/nsq_to_mongo/nsq_to_mongo.go
+++ apps/nsq_to_mongo/nsq_to_mongo.go
@@ -1,23 +1,87 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/bitly/go-nsq"
+	"github.com/deepglint/glog"
+	"labix.org/v2/mgo"
+	//"labix.org/v2/mgo/bson"
+	//"log"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/bitly/go-nsq"
+	"time"
 	//"github.com/bitly/nsq/util"
 )
 
+var (
+	topic      = flag.String("topic", "Normal", "nsq topic")
+	channel    = flag.String("channel", "world", "nsq channel to subscribe")
+	nsq_addr   = flag.String("address", "192.168.2.103:4153", "nsq address")
+	database   = flag.String("db", "hello", "data base name to insert data")
+	collection = flag.String("collection", "world", "collection of mongo db")
+	mongo_addr = flag.String("mongoadd", "192.168.2.103:27017", "address of mongo db")
+	user       = flag.String("user", "timeline", "user name for mongo db")
+	password   = flag.String("password", "deepdbdb", "password for mongo db")
+)
+
 func main() {
-	fmt.Println("hello world")
+	/// to check if the params valid
+	// if *topic == "" {
+	// 	log.Fatalln("the topic should not be null, please use --topic=... ")
+	// }
+	// if *channel == "" {
+	// 	log.Fatalln("the channel should not be null, please use --channel=... ")
+	// }
+	// // if *nsq_addr == "" {
+	// // 	log.Fatalln("the address should not be null, please use --address=... ")
+	// // }
+	// if *collection == "" {
+	// 	log.Fatalln("the collection should not be null, please use --collection=... ")
+	// }
+	// if *user == "" {
+	// 	log.Fatalln("the username to access mongo should not be nul, please use --user=...")
+	// }
+	// if *password == "" {
+	// 	log.Fatalln("the password should not be null, please use --password=...")
+	// }
+	// if *mongo_addr == "" {
+	// 	log.Fatalln("the mongodb address should not be null, please use --mongoadd=...")
+	// }
+	// if *database == "" {
+	// 	log.Fatalln("the database name should not be null, please use --db=...")
+	// }
+	///
+	/// connect to mongodb
+	println(*mongo_addr)
+	session, err := mgo.Dial(*mongo_addr)
+	for err != nil {
+		glog.Errorf("MongoDB Dial Error: %v", err)
+		glog.Errorf("waiting for 1 second ...")
+		time.Sleep(1000 * time.Millisecond)
+		session, err = mgo.Dial(*mongo_addr)
+	}
+	defer session.Close()
+	//
+	db := session.DB(*database)
+	// err = db.Login(*user, *password)
+	// if err != nil {
+	// 	glog.Errorf("MongoDB Login Error: %v", err)
+	// 	return
+	// }
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	table := db.C(*collection)
+	///
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	cfg := nsq.NewConfig()
 
-	consumer, err := nsq.NewConsumer("hello", "world", cfg)
+	consumer, err := nsq.NewConsumer(*topic, *channel, cfg)
 
 	if err != nil {
 		fmt.Println("Error")
@@ -25,11 +89,25 @@ func main() {
 
 	consumer.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
 		// handle the message
-		fmt.Println("%v", m)
+		// go routine to write the message into db
+		// var out = new(bson.D)
+		fmt.Printf("%s", string(m.Body))
+		//err := bson.Unmarshal(m.Body, &out)
+		var out map[string]interface{}
+		err := json.Unmarshal(m.Body, &out)
+		if err != nil {
+			glog.Errorf("Error to unmarshal the json")
+		}
+		err = table.Insert(out)
+		//db.Insert(&this)
+		if err != nil {
+			glog.Errorf("Error saving event: %s", err)
+			return err
+		}
 		return nil
 	}))
 
-	err = consumer.ConnectToNSQD("localhost:4150")
+	err = consumer.ConnectToNSQD(*nsq_addr)
 	if err != nil {
 		fmt.Println("Error connecting")
 	}
